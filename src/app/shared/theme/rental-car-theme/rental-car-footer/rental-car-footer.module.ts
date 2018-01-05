2b4816f7e060a63518b5fd6c100e21b87b489fb9
Jimmy Lee <jimmy.lee@careline.com.tw jimmy.lee@careline.com.tw jimmy.lee@careline.com.tw jimmy.lee@careline.com.twjimmy.lee@careline.com.tw>
2018-01-05 18:54:35 +0800

    auto switch header and footer component

---
+++ src/app/shared/theme/rental-car-theme/rental-car-footer/rental-car-footer.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RentalCarFooterComponent } from './rental-car-footer.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [RentalCarFooterComponent],
+  entryComponents: [
+    RentalCarFooterComponent
+  ]
+})
+export class RentalCarFooterModule { }
