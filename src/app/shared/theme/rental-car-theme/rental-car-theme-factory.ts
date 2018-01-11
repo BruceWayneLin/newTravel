2776e6714fc1c74b81414f899759948a051de97c
Jimmy Lee <jimmy.lee@careline.com.tw jimmy.lee@careline.com.tw jimmy.lee@careline.com.tw jimmy.lee@careline.com.twjimmy.lee@careline.com.tw>
2018-01-11 11:41:44 +0800

    RentalCar Theme  - use default footer component with the display set to hidden some component

--- src/app/shared/theme/rental-car-theme/rental-car-theme-factory.ts
+++ src/app/shared/theme/rental-car-theme/rental-car-theme-factory.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { LayoutDataSetting } from 'cl-layout/src/app/shared/layout/layout';
 import { RentalCarFooterComponent } from './rental-car-footer/rental-car-footer.component';
 import { RentalCarHeaderComponent } from './rental-car-header/rental-car-header.component';
+import { Footer } from 'cl-layout/src/app/shared/layout/footer/footer';
 
 @Injectable()
 export class RentalCarThemeFactoryFactory implements LayoutDataSetting.DynamicSwitchedComponent {
@@ -20,5 +21,15 @@ export class RentalCarThemeFactoryFactory implements LayoutDataSetting.DynamicSw
 
     // use default footer component when you return null
     // return null;
+
+    // use default footer component with the display set to hidden some component
+    const defulatFooterDisplaySetting: Footer.DisplaySetting = {
+      quickLink: false,
+      commonLink: false,
+      contactUs: {
+        customerService: false,
+      }
+    };
+    return defulatFooterDisplaySetting;
   }
 }
