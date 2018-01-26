581585bcb0133823447d091c94af5f0597e19a1e
skanda.lin <skanda.lin@careline.com.tw>
2018-01-26 14:22:52 +0800

    filter extra mark code erased

--- src/app/filter.ts
+++ src/app/filter.ts
@@ -9,12 +9,6 @@ import { Injectable, Pipe, PipeTransform } from '@angular/core';
 
 export class filterPipe implements PipeTransform {
   transform(value: any[], args: number): any {
-    // var returnData;
-    // val.filter(val => val['groupId'] = args).map(
-    //   value =>  returnData = value
-    // )
-    //
-    // return returnData;
     for(let i = 0; i <= value.length; i++) {
       if(value[i]['groupId']){
         if(value[i]['groupId'] == args) {
